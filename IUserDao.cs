8a08ff332a699160baf34e8534e9443a5fb9fe35
tatyana-balashowa <63149624+tatyana-balashowa@users.noreply.github.com>
2020-04-04 16:22:28 +0400

    Users,DAL

---
+++ IUserDao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Users.Entities;
+namespace Users.DAL
+{
+    public  interface IUserDao
+    {
+        IEnumerable<User> GetUsers();
+        int? Add(User user);
+        void Remove(int index);
+    }
+}
