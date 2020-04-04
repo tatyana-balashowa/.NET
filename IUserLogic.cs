535d9912ac837102ad593c08130b907d450a98a9
tatyana-balashowa <63149624+tatyana-balashowa@users.noreply.github.com>
2020-04-04 16:19:08 +0400

    IUserLOgic.cs UserLogic.cs
    Users.BLL

---
+++ IUserLogic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Users.Entities;
+namespace Users.BLL
+{
+    public interface IUserLogic
+    {
+        IEnumerable<User> GetUsers();
+        int? Add(User user);
+        void Remove(int index);
+    }
+}
---
+++ UserLogic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Users.DAL;
+using Users.Entities;
+namespace Users.BLL
+{
+    public class UserLogic : IUserLogic
+    {
+        private IUserDao userDao;
+        public UserLogic()
+        {
+            userDao = new UserDaoDB();
+        }
+        public int? Add(User user)
+        {
+            return userDao.Add(user);
+        }
+        public IEnumerable <User> GetUsers()
+        {
+            return userDao.GetUsers();
+        }
+        public void Remove (int index)
+        {
+            userDao.Remove(index);
+        }
+
+    }
+}
